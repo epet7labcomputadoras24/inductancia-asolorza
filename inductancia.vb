cbab3e047a84ca22eae5ddeb564a188c1a1d5823
agustin solorza <agustinsolorza.jobs@gmail.com>
2024-08-06 12:56:31 -0300

    subo codigo resuelto y funcional

---
+++ inductancia.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+    Sub Main()
+        Dim frecuencia As Double
+        Dim inductancia As Double
+        Dim impedancia As Double
+
+
+        Console.Write("Ingrese la frecuencia (Hz): ")
+        frecuencia = Console.ReadLine()
+        Console.Write("Ingrese el valor de la inductancia (H): ")
+        inductancia = Console.ReadLine()
+        impedancia = 2 * Math.PI * frecuencia * inductancia
+
+        Console.WriteLine("La impedancia inductiva es: " & impedancia & " ohmios")
+
+    End Sub
+End Module
